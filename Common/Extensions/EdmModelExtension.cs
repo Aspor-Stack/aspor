00419ce64cc3c0e894a1dc5771007d879b158e97
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-12-18 19:46:21 +0100

    Added virtual navigation properties

---
+++ Common/Extensions/EdmModelExtension.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.OData.Edm;
+
+namespace Common.Extensions
+{
+    public static class EdmModelExtension
+    {
+
+        public static IEdmModel AddVirtualNavigation(this IEdmModel model, string sourceSet, string targetSet, string name) 
+        {
+            var source = (EdmEntitySet)model.FindDeclaredEntitySet(sourceSet);
+            var target = (EdmEntitySet)model.FindDeclaredEntitySet(targetSet);
+            var sourceType = (EdmEntityType)source.Type.AsElementType();
+            var targetType = (EdmEntityType)target.Type.AsElementType();
+
+            var property = new EdmNavigationPropertyInfo();
+            property.TargetMultiplicity = EdmMultiplicity.Many;
+            property.Target = targetType;
+            property.ContainsTarget = false;
+            property.OnDelete = EdmOnDeleteAction.None;
+            property.Name = name;
+
+            source.AddNavigationTarget(sourceType.AddUnidirectionalNavigation(property), target);
+
+            return model;
+        }
+
+    }
+}
